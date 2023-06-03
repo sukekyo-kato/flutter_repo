ee7046bd9dce27970a4f46396f76a10aa0bd08ff
kyosuke.kato <5939882+sukekyo-kato@users.noreply.github.com>
2023-06-03 21:40:30 +0900

    feat: 🚀 add clashlytics

--- packages/firebase_sample/lib/main.dart
+++ packages/firebase_sample/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,13 +9,18 @@ import 'app.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(
-    ProviderScope(
-      child: App(),
-    ),
-  );
+  await runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+    runApp(
+      ProviderScope(
+        child: App(),
+      ),
+    );
+  }, (error, stackTrace) {
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
+  });
 }
